ecd7d0a2aaf3320455a93449643d32d1055c18a6
theje <theje@Thej>
2025-09-14 13:25:59 +0530

    Sep14

---
+++ Java_Programs/src/ex_03_Literals/Lab035_Non_Primitive_Literal.java
@@ -0,0 +1,13 @@
+package ex_03_Literals;
+
+public class Lab035_Non_Primitive_Literal {
+	
+	public static void main(String[] args) {
+        String name = "Pramod"; // String is bunch of char.
+        int[] arrays_of_items1 = new int[20];
+       //System.out.print(arrays_of_items1);
+        
+
+    }
+
+}
