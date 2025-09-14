090ddb6b74c4b4a7f4a6a0ac287b660d24eab7d8
theje <theje@Thej>
2025-09-14 11:33:45 +0530

    Initial Commit.

---
+++ Java_Programs/src/ex_01_Java_Basics/Lab001_Hello_World.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basics;
+
+public class Lab001_Hello_World {
+	public static void main(String[] args) {
+
+
+        System.out.println("Hello 1");
+        System.out.println("Hello 2");
+        System.out.println("Hello 3");
+        System.out.println("Hello 4");
+	}
+
+}
