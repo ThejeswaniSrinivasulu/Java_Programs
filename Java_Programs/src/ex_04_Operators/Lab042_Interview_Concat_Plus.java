48126ea16e189415312e6c95684b7d9110b82072
theje <theje@Thej>
2025-09-20 21:51:06 +0530

    Sep 20

---
+++ Java_Programs/src/ex_04_Operators/Lab042_Interview_Concat_Plus.java
@@ -0,0 +1,24 @@
+package ex_04_Operators;
+
+public class Lab042_Interview_Concat_Plus {
+	
+	public static void main (String[] args) {
+	
+	int a=10;
+	int b =20;
+	System.out.println(a+b);
+	
+	String first_name= "Thejeswani";
+	String Last_name="Kapuluru";
+	System.out.println(first_name+ " " +Last_name);
+	
+ //System.out.println(first_name-last_name);
+  //System.out.println(first_name*last_name);
+ //System.out.println(first_name/last_name);
+
+  // + -> behave differently with the data type.
+  // + -> operator overloading
+	
+	}
+
+}
---
+++ Java_Programs/src/ex_05_TypeCasting/Lab057_TypeCasting.java
@@ -0,0 +1,15 @@
+package ex_05_TypeCasting;
+
+public class Lab057_TypeCasting {
+	
+	public static void main(String[] args) {
+        byte b = 10;
+        // int a = b; // // Valid syntax - Implicit - Casting -> Widening ->
+        int a = (int) b; // Explicit Casting - Widening
+
+//        int a1 = 300;
+//        byte b1 = a1;
+
+    }
+
+}
